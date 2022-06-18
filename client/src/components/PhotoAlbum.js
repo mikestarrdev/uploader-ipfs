2a5b09fbcdcebdbfeae05ec5b96bdbc1a50c193e
Mike Starr <imhs@protonmail.com>
2022-06-18 11:43:41 -0400

    Added basic styling to PhotoAlbum component

--- client/src/components/PhotoAlbum.js
+++ client/src/components/PhotoAlbum.js
@@ -3,9 +3,19 @@ import styled from "styled-components";
 
 function PhotoAlbum() {
   const Album = styled.div`
-    display: flex;
-    flex-direction: row;
+    display: grid;
+    grid-template-columns: 25% 25% 25% 25%;
+    align-items: center;
     width: auto;
+    margin: auto;
+    padding: 1rem;
+  `;
+
+  const Photo = styled.div`
+    border: solid lightgray 2px;
+    align-items: center;
+    justify-content: center;
+    margin: 0.1rem;
   `;
 
   const [photos, setPhotos] = useState();
@@ -13,27 +23,32 @@ function PhotoAlbum() {
   useEffect(() => {
     fetch("https://api.pexels.com/v1/search?query=people", {
       headers: {
-        Authorization: "",
+        Authorization:
+          
       },
     })
       .then((resp) => resp.json())
       .then((data) => {
-        console.log(data.photos);
+        console.log(data);
         setPhotos(data.photos);
       });
   }, []);
 
   const renderPhotos = photos?.map((photo) => {
     return (
-      <div>
+      <Photo>
         <img src={photo.src.small} alt={photo.alt} key={photo.id} />
-      </div>
+        <p>
+          <strong>Uploaded by:</strong> {photo.photographer} <br />
+          <strong>Description:</strong> {photo.alt}
+        </p>
+      </Photo>
     );
   });
 
   return (
     <div>
-      <h3>Photos</h3>
+      <h3>(Album Title)</h3>
       <Album>{renderPhotos}</Album>
     </div>
   );
